467434123fbfee227cb5b030a66e090132d6e9f7
Afshin Zafari <afshin.zafari@it.uu.se>
2013-06-22 14:25:13 +0200

    Test results for Propagation message size.

--- ResStudio/Prototype/chol_fact.hpp
+++ ResStudio/Prototype/chol_fact.hpp
@@ -46,32 +46,26 @@ public:
     sprintf(gemm_str,"gemm");
 
 
-      for ( int i = 0; i< Nb ; i++) {
+    for ( int i = 0; i< Nb ; i++) {
 
-	//printf("@RECT CTX\n");
-	BEGIN_CONTEXT(A.Region(i,Nb-1,0,i-1) , A.RowSlice(i,0,i-1), A.ColSlice(i,i,Nb-1))  // 0.i*2
+	  BEGIN_CONTEXT(A.Region(i,Nb-1,0,i-1) , A.RowSlice(i,0,i-1), A.ColSlice(i,i,Nb-1))  // 0.i*2
 	  for(int l = 0;l<i;l++){
-	    //printf("@GEMM CTX,%d\n",l);
 	    BEGIN_CONTEXT_BY(l,A.ColSlice(l,i,Nb-1) , A.Cell(i,l), A.ColSlice(i,i,Nb-1))       // 0.i*2.L
 	      for (int k = i; k<Nb ; k++){
-		//printf("gemm %s\n",A(k,i)->getName().c_str());
-		AddTask (this,gemm_str, A(k,l) , A(i,l) , A(k,i) ) ;
+	        AddTask (this,gemm_str, A(k,l) , A(i,l) , A(k,i) ) ;
 	      }
 	    END_CONTEXT()
 	  }
-	END_CONTEXT()
+	  END_CONTEXT()
 
-	//printf("Chol %s\n",A(i,i)->getName().c_str());
-	AddTask (this, chol_str , A(i,i));
+	  AddTask (this, chol_str , A(i,i));
 
-	//printf("@PANEL CTX\n");
-	BEGIN_CONTEXT( A.Cell(i,i),NULL,A.ColSlice(i,i+1,Nb-1) )                         // 0.i*2+1
-	  for( int j=i+1;j<Nb;j++){
-	    //printf("pnlu %s\n",A(j,i)->getName().c_str());
-	    AddTask(this,pnl_str,A(i,i), A(j,i) ) ;
-	  }
-	END_CONTEXT()
-      }
+	  BEGIN_CONTEXT( A.Cell(i,i),NULL,A.ColSlice(i,i+1,Nb-1) )                         // 0.i*2+1
+	    for( int j=i+1;j<Nb;j++){
+	        AddTask(this,pnl_str,A(i,i), A(j,i) ) ;
+	    }
+	  END_CONTEXT()
+    }
 
   }
 
